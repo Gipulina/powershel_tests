c96e11075bc63884ac5beeca530f0984a63ccf1b
gipulina <georgi_palavecino@hotmail.com>
2022-05-16 19:20:59 -0300

    foreach loop is working

--- mobiles.ps1
+++ mobiles.ps1
@@ -37,13 +37,13 @@ $Mobiles=@(
 # $Mobiles | Where-Object { $_.Model -eq "Galaxy S22"} | Select-Object -Property 'Colors','Brand'
 
 # $Mobiles[0].Storage[0]
-# $Mobiles[0].Defaultapps.Item(0)
+# $Mobiles[0].Defaultapps.Item(1)
 
-$Mobiles[0].$Defaultapps.Item[0]
+# $Mobiles[0].$Defaultapps.Item()
 
-foreach($app in $Mobiles[0].$Defaultapps)
+foreach($app in $Mobiles[0].Defaultapps)
 {
+    'Version: '+ $app.Name 
     $app.Status
-    $app.Name
     $app.Version
 }
